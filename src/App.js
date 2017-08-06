19255bebfaa9e756bfdb20b13c77d6d05617db7c
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2017-08-06 15:11:18 -0400

    Initial commit with basic site setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to my personal portfolio</h2>
+        </div>
+        <p className="App-intro">
+          Site under development. Coming soon.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
